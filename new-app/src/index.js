c9bdac6a258f35fd2b537e99c758d14d824bc890
Yulia Gavris <gavris@uchi.ru>
2018-05-15 18:59:02 +0300

    save

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -2,112 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const data = [
-  {
-    "title": "Abstract",
-    "content": [
-      { 
-        "subtitle": null, 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "History",
-    "content": [
-      { 
-        "subtitle": "European contact", 
-        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae ultricies sapien. Maecenas placerat cursus ipsum, vel molestie est pulvinar eget. Phasellus quis placerat mauris, non tempus leo. Nulla accumsan mauris metus, eu volutpat neque sagittis eu. Proin finibus iaculis tellus. Morbi suscipit a elit at dictum. Praesent volutpat vulputate sagittis. Sed pharetra massa lectus, ac placerat felis pulvinar sed. Integer at egestas nulla. Vivamus placerat eros velit, et pharetra nulla auctor vitae. Sed magna neque, porta ut turpis sed, vulputate bibendum enim.", 
-        "img": null 
-      },
-      { 
-        "subtitle": "Major century", 
-        "text": "whatever", 
-        "img": null 
-      },
-      { 
-        "subtitle": "The revolution", 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "Governance",
-    "content": [
-      { 
-        "subtitle": null, 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "Geography",
-    "content": [
-      { 
-        "subtitle": "Subdivisions", 
-        "text": "whatever", 
-        "img": null 
-      },
-      { 
-        "subtitle": "Mount pelee", 
-        "text": "whatever", 
-        "img": null 
-      },
-      { 
-        "subtitle": "Geopolitics", 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "Economics",
-    "content": [
-      { 
-        "subtitle": null, 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "Demographics",
-    "content": [
-      { 
-        "subtitle": null, 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "Culture",
-    "content": [
-      { 
-        "subtitle": null, 
-        "text": "whatever", 
-        "img": null 
-      }
-    ]
-  },
-  {
-    "title": "References",
-    "content": [
-        { 
-          "subtitle": null, 
-          "text": "whatever", 
-          "img": null 
-        }
-    ]
-  }
+const data = [ 
+  "Abstract", 
+  ["History", "European contact", "Major century", "The revolution"], 
+  "Governance", 
+  ["Geography", "Subdivisions", "Mount pelee", "Geopolitics"], 
+  "Economics",
+  "Demographics",
+  "Culture",
+  "References",
 ]
 
 class WikiPage extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  renderNav () {
+    const navItems = data.map((elem, index) => {
+      console.warn(elem)
+      if (1 === 1) {
+        return (
+          <li className="nav__item" key={elem}>
+            <a className="nav__link" href="#">
+              {elem[0]}
+            </a>
+          </li>
+        )
+      } else {
+        return (
+          <li className="nav__item" key={elem}>
+            <a className="nav__link" href="#">
+              elem
+            </a>
+          </li>
+        )
+      }
+    })
+    return (
+      <ul className="nav">
+        {navItems}
+      </ul>
+    )
   }
 
   render() {
@@ -116,9 +52,7 @@ class WikiPage extends React.Component {
         <div className="header">
           Полосочка
         </div>
-        <div className="nav">
-          Меню
-        </div>
+        {this.renderNav()}
         <div className="article">
           Контент
         </div>
