9816a101336c404b384440a5ee5d1f3dc1bbf706
Yulia Gavris <gavris@uchi.ru>
2018-05-16 10:50:41 +0300

    save

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const data = [
   "References",
 ]
 
+const mainTitle = "Martinique"
+const fakeContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut ullamcorper leo. Sed id elit a lorem tempor eleifend nec et augue. Donec eget tortor ut ligula ultrices auctor quis ut massa. Pellentesque semper metus lacus, eget sollicitudin urna volutpat nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum vehicula eu augue ac varius. Nunc id lectus nisi. Maecenas vitae orci semper, ornare purus a, mattis enim. Donec facilisis eu mi vel tristique. Aenean lectus enim, mattis non lorem vel, vestibulum dignissim turpis. Aliquam a turpis sodales, mattis dolor non, tristique nibh. In bibendum magna ac orci dapibus commodo."
+
 class WikiPage extends React.Component {
   constructor(props) {
     super(props)
@@ -29,20 +32,19 @@ class WikiPage extends React.Component {
           .map((element) => {
             const sublink = "#" + element
             return (
-              <li className="nav__subitem" key={element}>
-                <a className="nav__sublink" href={sublink}>
+              <li className="nav-list__item" key={element}>
+                <a className="nav-list__link" href={sublink}>
                   {element}
                 </a>
               </li>
             )
           })
-        console.warn(items)
         return (
           <li className="nav__item" key={elem[0]}>
             <a className="nav__link" href={link}>
               {elem[0]}              
             </a>
-            <ul className="nav__item">
+            <ul className="nav-list">
               {items}
             </ul>
           </li>
@@ -65,6 +67,54 @@ class WikiPage extends React.Component {
     )
   }
 
+  renderArticle () {
+    const mainArticle = data.map((elem, index) => {
+      if (Array.isArray(elem)) {
+        const items = elem
+          .filter((element, index) => {
+            return index !== 0
+          })
+          .map((element) => {
+            return (
+              <div className="subarticle" id={element}>
+                <div className="article-wrap__subtitle">
+                  {element}
+                </div>
+                <div className="article-wrap__content">
+                  {fakeContent}
+                </div>
+              </div>
+            )
+          })
+        return (
+          <div className="article-wrap">
+            <div className="article-wrap__title" id={elem[0]}>
+              {elem[0]}
+            </div>
+            {items}
+          </div>   
+        )
+      } else {
+        return (
+          <div className="article-wrap">
+            <div className="article-wrap__title" id={elem}>
+              {elem}
+            </div>
+            <div className="article-wrap__content">
+              {fakeContent}
+            </div>
+          </div>          
+        )
+      }
+    })
+    return (
+      <div className="article">
+        {mainArticle}
+      </div>
+    )
+
+  }
+
   render() {
     return (
       <div className="page">
@@ -72,9 +122,7 @@ class WikiPage extends React.Component {
           Полосочка
         </div>
         {this.renderNav()}
-        <div className="article">
-          Контент
-        </div>
+        {this.renderArticle()}
       </div>
     );
   }
